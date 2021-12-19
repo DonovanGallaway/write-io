bfd94f69197a5b0950628cb385d3e1e66dbf5b12
Donovan Gallaway <donovangallaway@gmail.com>
2021-12-18 21:23:53 -0800

    added welcome message

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
 	import Header from './components/Header.svelte'
+	import Instructions from './components/Instructions.svelte'
+	let instructions
 </script>
 
 <Header/>
+<Instructions instructions={instructions}/>
---
+++ src/components/Instructions.svelte
@@ -0,0 +1,42 @@
+<script>
+    import {onMount} from 'svelte'
+    import {fade} from 'svelte/transition'
+    let instructions = false
+    onMount(()=> {
+        instructions = true
+    })
+    let showState = "Hide"
+    const hideInstruction = () => {
+        instructions = !instructions
+        if (instructions){
+            showState = "Hide"
+        } else {
+            showState = "Show"
+        }
+    }
+</script>
+
+<style>
+    div {
+        width: 50vw;
+        margin: auto;
+    }
+
+    p {
+        font-size: 24px;
+    }
+</style>
+
+<div>
+{#if instructions}
+    <div transition:fade={{duration:500}}>
+        <p><i>Welcome to the (very) unofficial "National Novel Writing Month App," affectionately dubbed "NaNoWriM.io" after the abbreviation for the event ("NaNoWriMo").<br/><br/>
+        The purpose of the app is to help you track and meet your quotas for NaNoWriMo or your writing in general, making sure you have a space to track your word counts and words. It should be noted that this assumes a relatively traditional writing style and an actual desire to write more and set quotas. After all, cheaters never prosper and it's hard to help someone who writes with pen and paper using word processing tools...<br/><br/>
+        Anyway! Hope you enjoy!</i></p>
+    </div>
+    {:else}
+<div></div>
+{/if}
+
+<button on:click={hideInstruction}>{showState}</button>
+</div>
