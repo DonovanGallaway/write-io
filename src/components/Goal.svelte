28ec802b05722d4d63d0f983a36161c451d58712
Donovan Gallaway <donovangallaway@gmail.com>
2021-12-22 20:26:59 -0800

    more animation tweaking

--- src/components/Goal.svelte
+++ src/components/Goal.svelte
@@ -1,6 +1,6 @@
 <script>
     import GoalForm from './GoalForm.svelte'
-    import {fly,fade, slide} from 'svelte/transition'
+    import {fade, fly} from 'svelte/transition'
     export let goal
     export let getGoals
     $: wordCount = goal.text.split(' ').length - 1
@@ -95,15 +95,15 @@
 {/if}
 
 {#if stillWriting}
-<textarea transition:slide={{duration:1000}} bind:value={goal.text}/>
-<div transition:slide={{duration:1000}}>
+<textarea out:fly={{x: 1000, duration:1000}} in:fly={{x: 1000, duration:1000,delay: 1000}} bind:value={goal.text}/>
+<div out:fly={{x: 1000, duration:1000}} in:fly={{x: 1000, duration:1000,delay: 1000}}>
 <button on:click={saveGoal}>Save</button>
 <button on:click={toggleEdit}>{editButton}</button>
 <button class="delete-btn" on:click={deleteGoal}>Delete Goal</button>
 </div>
 {:else}
-<p transition:slide={{duration:1000}}>{goal.text}</p>
-<div transition:slide={{duration:1000}}>
+<p out:fly={{x: 1000, duration:1000}} in:fly={{x: 1000, duration:1000,delay: 1000}}>{goal.text}</p>
+<div out:fly={{x: 1000, duration:1000}} in:fly={{x: 1000, duration:1000,delay: 1000}}>
 <button class='keep-writing' on:click={() => {stillWriting = true}}>Keep Writing!</button>
 <button class='delete-btn' on:click={deleteGoal}>Delete Goal</button>
 </div>
