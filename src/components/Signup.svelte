ebc47fd2d07d4fd7dc985670e8917e763b3b5cc1
Donovan Gallaway <donovangallaway@gmail.com>
2023-12-21 23:57:07 -0800

    prepare changes for auth

--- src/components/Signup.svelte
+++ src/components/Signup.svelte
@@ -1,18 +1,31 @@
 <script>
+
+    export let login
+
+    const base_url = "https://nanowrimo-backend-ed979ddf8fb7.herokuapp.com/auth"
+
+    const url = login ? base_url + "/login" : base_url + "/signup"
+
+    let password_matches = true
+
     let formData = {
         username: "",
         password: ""
     }
 
-    const url = "https://nanowrimo-backend-ed979ddf8fb7.herokuapp.com/auth/signup"
+    let confirm_password
 
-    const handleSubmit = async (formData) => {
+    const handleSubmit = async () => {
+        if (formData.password != confirm_password){
+            password_matches = false
+            return
+        }
         await fetch(url, {
             method: "post",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({username, password})
+            body: JSON.stringify(formData)
         })
     }
 </script>
@@ -22,7 +35,15 @@
         <input type='text' name='username' bind:value={formData.username}/>
     </label>
     <label for="password">Password
-        <input type='text' name='password' bind:value={formData.password}/>
+        <input type='password' name='password' bind:value={formData.password}/>
     </label>
+    {#if login}
+    <label for="confirm password">Confirm Password
+        <input type="password" name="confirm_password" bind:value={confirm_password}/>
+    </label>
+    {#if !password_matches}
+        <p>Passwords do not match</p>
+    {/if}
+    {/if}
     <input type='submit' value='Sign Up'/>
 </form>
